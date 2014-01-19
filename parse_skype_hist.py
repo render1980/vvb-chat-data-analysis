da3e2947ac451bb80e1747adf6314dea1db342a5
render1980 <render1980@gmail.com>
2014-01-19 21:17:50 +0400

    * add save to cvs * change README

--- parse_skype_hist.py
+++ parse_skype_hist.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import re
-	
+import csv
+
 class VVBParser:
 	"""
 	Class for parsing unstructured text with messages of Skype chat format 
@@ -12,17 +13,14 @@ class VVBParser:
 		self.records = []
 		err_lines = []
 
-	# help methods
 	def __parse_line(self, line):
 		m = re.match(u"^\W(\d+).(\d+).(\d+)\W\s(\d+:\d+:\d+)\W\s([\S\s]+?):\s([\s\S]+)", line)
 		if m:
 			return ("20" + m.group(3) + "-" + m.group(2) + "-" + m.group(1) + " " + m.group(4),
 					m.group(5), m.group(6))
 
-	# model logic
 	def __save_line(self, groups):
 		if groups:
-			#print (groups[0] + " | " + groups[1] + " | " + groups[2] + "\n")
 			self.records.append((groups[0], groups[1], groups[2]))
 
 	def parse(self, file_name):
@@ -37,5 +35,15 @@ class VVBParser:
 			self.__save_line(groups)
 		return self.records
 
+	def save_to_csv(self, lines, file_name):
+		f = open(file_name, 'wb')
+		wr = csv.writer(f, quoting=csv.QUOTE_ALL)
+		wr.writerow(('RECTIME', 'NICK', 'MESSAGE'))
+		for line in lines:
+			wr.writerow(line)
+		f.close()
+
 # Main workflow #
-lines = VVBParser().parse('./data/vvb_chat_hist.txt')
+parser = VVBParser()
+lines = parser.parse('./in/vvb_chat_hist.txt')
+parser.save_to_csv(lines, './out/vvb_chat_hist.csv')
